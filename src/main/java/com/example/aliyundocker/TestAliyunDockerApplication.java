106b397963be48f7dc90f767f9e38465c563fac8
wuxiaodong <308200039@qq.com>
2018-11-09 16:51:32 +0800

    first version

---
+++ src/main/java/com/example/aliyundocker/TestAliyunDockerApplication.java
@@ -0,0 +1,14 @@
+package com.example.aliyundocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAliyunDockerApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TestAliyunDockerApplication.class, args);
+    }
+}
